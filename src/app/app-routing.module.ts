5d203d2c2e464fe348f1b941fa78b38257cc3005
Alvaro <alvaro11xd93@gmail.com>
2019-03-24 18:56:00 -0600

    separacion en componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: SignupComponent },
+  /*{ path: 'path2', component: Name2Component },
+  { path: 'path3', component: Name3Component },
+  { path: 'path4', component: Name4Component },
+  { path: '**', component: PageNotFoundComponent },*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+//export const routingComponents = [HomeComponent]
