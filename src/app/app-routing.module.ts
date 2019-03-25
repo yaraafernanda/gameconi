4bc406d1a7f5c645e23d5620f2533d96648eb200
YaraAngel <yaraab95@gmail.com>
2019-03-24 23:14:17 -0600

    Vistas Profile, ProfileSettings y GamePlayed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileSettingsComponent } from './components/profile-settings/profile-settings.component';
+import { GameplayedComponent } from './components/gameplayed/gameplayed.component';
+import { GameComponent } from './components/game/game.component';
+import { GameplayComponent } from './components/gameplay/gameplay.component';
+import { GameoverComponent } from './components/gameover/gameover.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  /*{ path: 'path2', component: Name2Component },
-  { path: 'path3', component: Name3Component },
-  { path: 'path4', component: Name4Component },
-  { path: '**', component: PageNotFoundComponent },*/
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: ProfileSettingsComponent },
+  { path: 'gamesplayed', component: GameplayedComponent },
+  { path: 'game', component: GameComponent },
+  { path: 'gameplay', component: GameplayComponent },
+  { path: 'gameover', component: GameoverComponent }
 ];
 
 @NgModule({
