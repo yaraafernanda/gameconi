8ae6787396d8b4095cb72a954207b3317ee3822a
Alvaro <alvaro11xd93@gmail.com>
2019-04-19 11:40:48 -0500

    folders and usuario class

---
+++ src/app/class/User.ts
@@ -0,0 +1,15 @@
+export class Usuario { 
+    /*id:number;
+    nombre: string;
+    edad: number;
+    calificacion: number;
+    carrera: string;
+    sexo: string;*/
+    constructor(
+        public id:number, 
+        public username: string,
+        public email: string,
+        public password: string){
+
+        }
+}
