a067637c911f7f40800a94db07605178674c1aa1
Alvaro <alvaro11xd93@gmail.com>
2019-04-20 14:36:06 -0500

    signUp

--- src/app/services/usuarios/usuario.service.ts
+++ src/app/services/usuarios/usuario.service.ts
@@ -7,16 +7,47 @@ import { User } from 'src/app/class/User';
 })
 export class UsuarioService {
   //https://api.myjson.com/bins/wse9o
-  users:User[];
+  private users:User[];
   usersChange = new Subject<User[]>();
+  private lastId:number=1;
   constructor() { }
   private urlJSON="https://api.myjson.com/bins/wse9o";
+  //leerDatosDelJSON();
   async leerDatosDelJSON() {
     let response = await fetch(this.urlJSON);
     if(response.status != 200 ) return [];
     let arreglo =  await response.json();
     this.users=arreglo.slice();
     this.usersChange.next(this.users.slice());
+    this.lastId=this.users.length+1;
   }
-  
+  getNextId():number{
+    return this.lastId;
+  }
+  getUsers():User[]{
+    return this.users;
+  }
+  createUser(user:User){
+    console.log('Usuario a crear',user);
+    this.users.push(user);
+    console.log('USUARIOS',this.users);
+    // 1. Crear XMLHttpRequest object
+    let xhr = new XMLHttpRequest();
+    // 2. Configurar:  PUT actualizar archivo
+    xhr.open('PUT', this.urlJSON);
+    // 3. indicar tipo de datos JSON
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    // 4. Enviar solicitud a la red
+    xhr.send(JSON.stringify(this.users));
+    // 5. Una vez recibida la respuesta del servidor
+    xhr.onload = function () {
+        if (xhr.status != 200) { // analizar el estatus de la respuesta HTTP 
+            // Ocurrió un error
+            alert(xhr.status + ': ' + xhr.statusText); // e.g. 404: Not Found
+        } else {
+             console.log(xhr.responseText); // Significa que fue existoso
+        }
+    };
+    this.usersChange.next(this.users.slice());
+  }
 }
