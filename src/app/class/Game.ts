bdb96599e281b18a374d1489d02da8da0b0c62d8
mrianasd <is703392@iteso.mx>
2019-04-28 15:19:35 -0500

    gameplay changes

---
+++ src/app/class/Game.ts
@@ -0,0 +1,14 @@
+export class Game{ 
+    /*id:number;
+    nombre: string;
+    edad: number;
+    calificacion: number;
+    carrera: string;
+    sexo: string;*/
+    constructor(
+        public id: number,
+        public name: string,
+        public searches: number,
+        public imgSrc: string) {
+        }
+}
--- src/app/components/gameplay/gameplay.component.ts
+++ src/app/components/gameplay/gameplay.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from 'src/app/services/games/higher-lower/game.service';
+import { Game } from 'src/app/class/Game';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-gameplay',
@@ -7,9 +11,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameplayComponent implements OnInit {
 
-  constructor() { }
+  games: Game[] = [];
+  game1: Game;
+  game2: Game;
+  points: number;
+  subscript: Subscription;
+  constructor(private gService: GameService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.gService.reset();
+    this.gService.sortGames();
+    // this.games.sort(() => Math.random() - 0.5);
+    this.getCurrentGamePlay();
+    this.subscript = this.gService.updatePoints.subscribe(
+      (n: number) => { this.points = n; }
+    );
+    }
+
+
+  getCurrentGamePlay() {
+    this.games = this.gService.getGames();
+    console.log('games:', this.games, this.games.length);
+    if (this.games.length === undefined) {
+      this.router.navigate(['gameover'] );
+      console.log('you finished');
+    } else {
+      this.game1 = this.games[0];
+      this.game2 =  this.games[1];
+    }
+
   }
+
+  isHigher() {
+    if (this.game2.searches > this.game1.searches) {
+      this.gService.sumPoints();
+      //to get the nex game
+      this.getCurrentGamePlay();
+    } else {
+      this.endGame();
+    }
+  }
+
+  isLower() {
+    if (this.game2.searches < this.game1.searches) {
+      this.gService.sumPoints();
+      //to get the nex game
+      this.getCurrentGamePlay();
+    } else {
+      this.endGame();
+    }
+  }
+  endGame() {
+    this.router.navigate(['gameover']);
+  }
+
 
 }
