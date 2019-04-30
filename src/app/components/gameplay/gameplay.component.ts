4f55be04f88e0518a0cb984d588a03a6e9d24a44
mrianasd <is703392@iteso.mx>
2019-04-30 15:28:41 -0500

    trying to solve conflict

--- src/app/components/gameplay/gameplay.component.ts
+++ src/app/components/gameplay/gameplay.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GameService } from '../../services/games/higher-lower/game.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { VideoGame } from '../../class/VideoGame';
-import { User } from '../../class/User';
-import { Partida } from '../../class/Partida';
-import { AuthService } from '../../services/auth.service';
+import { Game } from '../../class/Game';
 
 @Component({
   selector: 'app-gameplay',
@@ -13,33 +10,15 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./gameplay.component.css']
 })
 export class GameplayComponent implements OnInit {
-  gamesplayed: Partida;
-  logged: boolean = false;
-  user: User;
 
-  games: VideoGame[] = [];
-  game1: VideoGame;
-  game2: VideoGame;
+  games: Game[] = [];
+  game1: Game;
+  game2: Game;
   points: number;
-
-  loginS: Subscription;
   subscript: Subscription;
-
-  constructor(private authService: AuthService, private gService: GameService, private router: Router, private route: ActivatedRoute) { }
+  constructor(private gService: GameService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.gService.leerJSON();
-    // checa si esta loggeado para poder jugar
-    this.logged = this.authService.isAuthehticated();
-    this.loginS = this.authService.loginStatusChange.subscribe((usuario:User)=>{
-      console.log('ENTRO');
-      this.logged = true;
-        this.user = this.authService.user;
-        console.log('user:', this.user);
-        console.log('USUARIO LOGEADO',usuario);
-    });
-    //this.gamesplayed.user_id = this.user.id;
-    // toma los datos de los juegos
     this.gService.reset();
     this.gService.sortGames();
     // this.games.sort(() => Math.random() - 0.5);
@@ -52,8 +31,10 @@ export class GameplayComponent implements OnInit {
 
   getCurrentGamePlay() {
     this.games = this.gService.getGames();
+    console.log('games:', this.games, this.games.length);
     if (this.games.length === undefined) {
-      this.endGame();
+      this.router.navigate(['gameover'] );
+      console.log('you finished');
     } else {
       this.game1 = this.games[0];
       this.game2 =  this.games[1];
@@ -64,7 +45,7 @@ export class GameplayComponent implements OnInit {
   isHigher() {
     if (this.game2.searches > this.game1.searches) {
       this.gService.sumPoints();
-      //to get the nex game
+      // to get the nex game
       this.getCurrentGamePlay();
     } else {
       this.endGame();
@@ -74,24 +55,15 @@ export class GameplayComponent implements OnInit {
   isLower() {
     if (this.game2.searches < this.game1.searches) {
       this.gService.sumPoints();
-      //to get the nex game
+      // to get the nex game
       this.getCurrentGamePlay();
     } else {
       this.endGame();
     }
   }
   endGame() {
-     if (this.points === undefined) {
-       this.points = 0;
-       console.log("score: ", this.points);
-     }
-    // TODO: sacar el oponente
-    this.gamesplayed = new Partida(this.gService.getnextId(),0,
-    this.user.id, this.points, 0,0,this.user.id,0,1);
-    console.log('gameplayed: ', this.gamesplayed);
-    this.gService.addGamePlayed(this.gamesplayed);
     this.router.navigate(['gameover']);
-}
+  }
 
 
 }
